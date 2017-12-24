cdbb6da91b7329d7e308a28923d26307be75e5ca
Ryan Buxton <rbuxton1@gmail.com>
2017-12-24 12:42:43 -0700

    Does this look good

--- index.php
+++ index.php
@@ -11,18 +11,25 @@
       <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
         <img class="w3-image" src="https://1.bp.blogspot.com/-ZbY05reBEsk/Wc2K8GSQCkI/AAAAAAABCw0/fs-_yvh2c38W3wEFjRyUHHNFUwYjYAhigCLcBGAs/s1600/13.jpg" alt="ryan" width="1500" height="800">
         <div class="w3-display-middle w3-margin-top w3-center">
-          <h1 class="w3-xxlarge w3-text-black"><span class="w3-padding w3-black w3-opacity-min"><b>Hi, Im Ryan. I like computers.</b></span></h1>
+          <h1 class="w3-xxlarge w3-text-black"><span class="w3-padding w3-black w3-opacity-min"><b>Hi, I'm Ryan. I like computers.</b></span></h1>
         </div>
       </header>
-
-      <hr><br>
+      <hr>
 
       <!-- Body -->
+      <h1 class="w3-xlarge">ryan@ryan-desktop:~$ ryan -about </h1>
       <div class='w3-center'>
         <p class='w3-large'>
-          What do I do?
+          I'm a computer science student in Colorado. I am a highschool senior, and plan on going to college to further my carrer. I have been taught Java and Andriod Studio's "almost Java" in school and have self taught myself PHP, HTML, basic MySQL, basic C. I also know a lot of Ubuntu tricks lending to my work as our schools Cyber Patriot Ubuntu specialist. This site has been entirely developed on my own using all these skills I have learned. All the code for this site can be found <a href='http://www.github.com/rbuxton1/ryanbuxton-website'>here</a>.
         </p>
       </div>
+      <h1 class='w3-xlarge'>ryan@ryan-desktop:~$ ryan -status </h1>
+      <div class='w3-center'>
+        <p class='w3-large'>
+          As of right now I have several personal and nonpersonal projects I am working on. The largest of which is my computer science senior project with my good friend <a href='http://www.github.com/cheezosis/'>Curtis Worthy</a>. The aim is to provide a safe for work arts database that our schools photography department can use to store students work. The project is called Student Art Archive, or SAA for short. I additionally am working on Show Helper, a program I wrote for my mom to use in the model horse hobby to helpmake judging shows easier. I am currently working on expanding the project to provide a database backend for the users so that the entery process is much easier. 
+        </p>
+      </div>
+
 
       <!-- Footer -->
       <footer class="w3-container w3-theme-dark w3-padding-16">
