dde2bf0ccd2cda391ea8d92ad6fd17cc73125706
Ryan Buxton <rbuxton1@gmail.com>
2017-12-24 11:56:07 -0700

    Filling it out

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+  <title>Ryan Buxton</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+  <body>
+
+      <!-- Header -->
+      <header class="w3-container w3-theme w3-padding" id="myHeader">
+        <div class="w3-center">
+          <h1 class="w3-xxxlarge w3-animate-bottom">Hi, Im Ryan. I like computers.</h1>
+          <div class="w3-padding-32">
+            <button class="w3-btn w3-xlarge w3-dark-grey w3-hover-light-grey" onclick="location.href='dash/login.php'" style="font-weight:900;">LOGIN</button>
+          </div>
+        </div>
+      </header>
+      <hr><br>
+
+      <!-- Footer -->
+      <footer class="w3-container w3-theme-dark w3-padding-16">
+        <p>
+          <div class="w3-center">
+            <p> <a href='http://www.github.com/rbuxton1'> << Ryan Buxton 2017 >> </a></p> 
+          </div>
+        </p>
+        <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
+          <span class="w3-text w3-theme-light w3-padding">Go To Top</span> 
+          <a class="w3-text-white" href="#myHeader"><span class="w3-xlarge">
+          <i class="fa fa-chevron-circle-up"></i></span></a>
+        </div>
+      </footer>
+    </body>
+  </html>
