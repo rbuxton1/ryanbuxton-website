63b8126fd81a34a4a86adac9498492e30b301cf8
Ryan <rbuxton1@gmail.com>
2018-03-09 09:43:19 -0700

    updates

--- presentations.php
+++ presentations.php
@@ -48,15 +48,19 @@
 
           <div class='w3-card w3-third w3-hover-shadow'>
             <header class="w3-container w3-orange">
-              <h1>Pedal Cart</h1>
+              <h1 class="w3-text-white">The Wike Bagon</h1>
             </header>
             <div class="w3-container">
               <p>stuff</p>
             </div>
             <footer class="w3-container w3-orange">
-              <h5>Brandon Chansamone</h5>
+              <h5 class="w3-text-white">Brandon Chansamone and Zack Iraqi</h5>
             </footer>
           </div>
+
+        </div>
+        <br>
+        <div class='w3-row-padding w3-container'>
 
           <div class='w3-card w3-third w3-hover-shadow'>
             <header class="w3-container w3-blue">
@@ -70,8 +74,21 @@
             </footer>
           </div>
 
+          <div class="w3-card w3-third w3-hover-shadow">
+            <header class="w3-container w3-green">
+              <h1>Student Art Archive (SAA)</h1>
+            </header>
+            <p class="w3-container">
+              <a href="http://www.github.com/rbuxton1/saa">Student Art Archive</a> (shortened to SAA) is a joint project with my friend <a href="http://www.github.com/cheezosis/">Curtis Worthy</a>, which also serves as our senior project for our schools computer science pathway. The project is an online database for art students to upload their work to. We took what I knew from working on the preliminary versions of online Show Helper and used that to create the basic outline of the project. From there we worked further on making it run smoother and on beautifying the whole thing. I also took some time to write the wiki as I installed a version on my home server for testing. At the time of writing, it is not entirely complete, but it is functional.
+            </p>
+            <footer class="w3-container w3-green">
+              <h5>Ryan Buxton, Curtis Worthy</h5>
+            </footer>
+          </div>
+
         </div>
     </div>
+    <br>
   </body>
   <footer class="w3-container w3-theme-dark w3-padding-16">
     <p>
@@ -79,10 +96,5 @@
         <p> <a href='http://www.github.com/rbuxton1'> << Ryan Buxton 2017 >> </a></p>
       </div>
     </p>
-    <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
-      <span class="w3-text w3-theme-light w3-padding">Go To Top</span> 
-      <a class="w3-text-white" href="#home"><span class="w3-xlarge">
-      <i class="fa fa-chevron-circle-up"></i></span></a>
-    </div>
   </footer>
 </html>
