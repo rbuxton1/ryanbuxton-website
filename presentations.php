1730e6641691e6a169d4e0a70ff60a1e7fb625c0
Ryan <rbuxton1@gmail.com>
2018-03-09 09:09:34 -0700

    wokring on adding stuff for senior project

---
+++ presentations.php
@@ -0,0 +1,76 @@
+<html>
+  <title>Senior Project</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+  <body>
+
+    <!-- Header -->
+    <header class="w3-container w3-theme w3-padding" id="myHeader">
+      <div class="w3-center">
+        <h1 class="w3-xxxlarge w3-animate-top">SENIOR PROJECT 2018</h1>
+      </div>
+    </header>
+    <hr><br>
+
+    <!--Body-->
+    <div class="w3-center">
+      <p>
+        Senior project 2018 flavor text, we hope youll come
+      </p>
+        <div class='w3-row-padding w3-container'>
+
+          <div class='w3-card w3-third w3-hover-shadow'>
+            <header class="w3-container w3-purple">
+              <h1>Strobe Tuner</h1>
+            </header>
+            <div class="w3-container">
+              <p>Device that is able to visually show how flat or sharp an instrument is to the user.</p>
+            </div>
+            <footer class="w3-container w3-purple">
+              <h5>Aman Tewolde</h5>
+            </footer>
+          </div>
+
+          <div class='w3-card w3-third w3-hover-shadow'>
+            <header class="w3-container w3-pink">
+              <h1>Calendar Buddy</h1>
+            </header>
+            <div class="w3-container">
+              <p>This app will schedule your entire life for you at the push of a button.</p>
+            </div>
+            <footer class="w3-container w3-pink">
+              <h5>Ryan McCormick</h5>
+            </footer>
+          </div>
+
+          <div class='w3-card w3-third w3-hover-shadow'>
+            <header class="w3-container w3-orange">
+              <h1>Pedal Cart</h1>
+            </header>
+            <div class="w3-container">
+              <p>stuff</p>
+            </div>
+            <footer class="w3-container w3-orange">
+              <h5>Brandon Chansamone</h5>
+            </footer>
+          </div>
+
+          <div class='w3-card w3-third w3-hover-shadow'>
+            <header class="w3-container w3-blue">
+              <h1>Flagpoles and Goal</h1>
+            </header>
+            <div class="w3-container">
+              <p>stuff</p>
+            </div>
+            <footer class="w3-container w3-blue">
+              <h5>Zeke, Khalid, and Deependra</h5>
+            </footer>
+          </div>
+
+        </div>
+    </div>
+  </body>
+</html>
