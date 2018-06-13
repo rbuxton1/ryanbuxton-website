63d6343e1dd59dd3b66ad1d6e68e446481d99858
Ryan <rbuxton1@gmail.com>
2018-06-12 23:19:43 -0600

    ugh

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <body>
 
       <!-- Header -->
-      <header class="w3-display-container w3-content w3-wide" style="width:100%;" id="home">
+      <header class="w3-display-container w3-content w3-wide" style="width:inherit;" id="home">
         <img class="w3-image" src="https://1.bp.blogspot.com/-ZbY05reBEsk/Wc2K8GSQCkI/AAAAAAABCw0/fs-_yvh2c38W3wEFjRyUHHNFUwYjYAhigCLcBGAs/s1600/13.jpg" alt="ryan" style="width:100%;">
         <div class="w3-display-topmiddle w3-margin-top w3-center">
           <br><br><br><br><br>
