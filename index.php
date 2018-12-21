62d6eaffc4dfb4ee06817fabba6b441512f6a44c
Ryan Buxton <rbuxton1@gmail.com>
2018-12-21 02:42:23 -0700

    updated
    Added more recent text and new background images without a horribly out of date image of myself.

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
   <body>
 
       <!-- Header -->
-      <header class="w3-display-container w3-content w3-wide" style="min-width:1500; width:100%;" id="home">
-        <img class="w3-image" src="https://1.bp.blogspot.com/-ZbY05reBEsk/Wc2K8GSQCkI/AAAAAAABCw0/fs-_yvh2c38W3wEFjRyUHHNFUwYjYAhigCLcBGAs/s1600/13.jpg" alt="ryan" style="width:100%;">
+      <header class="w3-display-container" id="home">
+        <img src="assets/mountains.jpg" alt="ryan" style="width:100%;">
         <div class="w3-display-topmiddle w3-margin-top w3-center">
           <br><br><br><br><br>
           <h1 class="w3-xxlarge w3-text-black"><b><span class="w3-left w3-padding w3-black w3-opacity-min typewrite" data-period="7000" data-type='[ "Hi, Im Ryan.", "I like computers.", "I make cool stuff with them."]'></span></b></h1>
@@ -22,13 +22,13 @@
       <div class='w3-container w3-margin'>
         <h1 class="w3-xlarge">ryan@ryan-desktop:~$ ryan -about </h1>
         <p class='w3-large w3-margin-left'>
-          I am a college student at CU Denver studying computer science. So far I have studied Java and Andriod Studio's "almost Java" in school, and have I have taught myself PHP, HTML, basic MySQL, basic C. I will be learning lots of C++ in my college carrer. I also know a lot of Ubuntu tricks and am my high school's Cyber Patriot Ubuntu specialist. This site has been developed using these skills I have learned. All the code for this site can be found <a href='http://www.github.com/rbuxton1/ryanbuxton-website'>here</a>.
+          I am a college student at CU Denver studying computer science. So far I have studied Java and C++, and have I have taught myself PHP, HTML, basic MySQL, basic C. I will be learning lots of C++ in my college carrer. I also know a lot of Ubuntu tricks and am my high school's Cyber Patriot Ubuntu specialist. This site has been developed using these skills I have learned. All the code for this site can be found <a href='http://www.github.com/rbuxton1/ryanbuxton-website'>here</a>. I am also Amateur radio licensed with call sign KE0QIP. I dont talk much but you might see me sometime.
         </p>
       </div>
       <div class='w3-container w3-margin'>
         <h1 class='w3-xlarge'>ryan@ryan-desktop:~$ ryan -status </h1>
         <p class='w3-large w3-margin-left'>
-          I am currently working on several projects, the largest of which is my computer science senior project with my good friend, <a href='http://www.github.com/cheezosis/'>Curtis Worthy</a>. Our objective is to build a safe for work arts database for our school's photography department to store student work. The project is called Student Art Archive, or SAA for short. Additionally, I am working on Show Helper, which is a program I wrote for my mom to facilitate model horse show judging. I am currently working on expanding that project to include a database back end.
+          I am currently working on several projects, and they all can be veiwed <a href="http://www.github.com/rbuxton1">on my github</a>. The projects range from a Discord chat bot that I made for my personal Discord to the online registration system for NaMoPaiMo. I am also working on a rewrite of Show Helper, but I have chosen to not make that code public yet.
         </p>
       </div>
       <div class='w3-container w3-margin'>
@@ -46,7 +46,7 @@
           <div class="w3-third w3-container w3-green">
             <h1 class="w3-xxlarge">Student Art Archive (SAA)</h1>
             <p class="w3-large">
-              <a href="http://www.github.com/rbuxton1/saa">Student Art Archive</a> (shortened to SAA) is a joint project with my friend <a href="http://www.github.com/cheezosis/">Curtis Worthy</a>, which also serves as our senior project for our schools computer science pathway. The project is an online database for art students to upload their work to. We took what I knew from working on the preliminary versions of online Show Helper and used that to create the basic outline of the project. From there we worked further on making it run smoother and on beautifying the whole thing. I also took some time to write the wiki as I installed a version on my home server for testing. At the time of writing it is about as complete as its gonna get without anyone taking any interest in it. Hopefully we will have it set up with out highschool so that a live version is available. Stay tuned!
+              <a href="http://www.github.com/rbuxton1/saa">Student Art Archive</a> (shortened to SAA) is a joint project with my friend <a href="http://www.github.com/cheezosis/">Curtis Worthy</a>, which also serves as our senior project for our schools computer science pathway. The project is an online database for art students to upload their work to. We took what I knew from working on the preliminary versions of online Show Helper and used that to create the basic outline of the project. From there we worked further on making it run smoother and on beautifying the whole thing. I also took some time to write the wiki as I installed a version on my home server for testing. At the time of writing it is about as complete as its gonna get without anyone taking any interest in it. A version is running at our highschool, unfortunately it is only an in-house version and requires one to be on the schools network.
             </p>
           </div>
           <div class="w3-third w3-container w3-blue">
@@ -55,15 +55,28 @@
               <a href="http://www.github.com/rbuxton1/namopaimo-entry">NaMoPaiMo entry</a> is another project that I worked for my mom to create. By far one of the more simple things Ive done, its a basic registration page for a competition my mom runs. The user inputs some data about themselves and stuff that they plan to do, and it takes all that and puts it into a MySQL database. More info about what NaMoPaiMo is can be found <a href="http://braymere.blogspot.com/p/national-model-painting-month-namopaimo.html">here</a>. At the time of writing, there are ~225 entries and they come from ~19 different countries. This project will be running until the end of January 2018, as thats when the registration closes. You can actually see the live page and maybe even enter yourself right <a href="http://ryanbuxton.com/namopaimo/">here</a>. Theres some cool prizes (especially if you're into little model horses)!
             </p>
           </div>
+        </div>
+        <div class='w3-row w3-border'>
           <div class="w3-third w3-container w3-blue-gray">
             <h1 class="w3-xxlarge">Black Nova</h1>
             <p class="w3-large">
               <a href="http://www.github.com/rbuxton1/black-nova">Black Nova</a> is a video game I am wokring with another close friend, Joseph Ahn, to create. All the programming is done by yours truely and all the art is done by him. So far not much has been done with it, but we have huge dreams of what we want to happen in it. Look forward to it! It should be really cool, at least to us. Were including some random generated RPG elements into it and a system by which adding a new weapon is as easy as editing a text file to make it as accessable as possible.
             </p>
           </div>
+          <div class="w3-third w3-container w3-teal">
+            <h1 class="w3-xxlarge">Mechapod</h1>
+            <p class="w3-large">
+              Mechapod is the Discord chat bot I use on my personal Discord server. It is a rewrite of an older version I had where everything was hard wired to my server, where as this newer version has many upgrades allowing it be used on other servers. The headline feature of Mechapod is its ability to create on the fly <a href="https://en.wikipedia.org/wiki/Shitposting">"shitposts"</a> from user inserted templates and sets of words. This was heavily inspired by a Tumblr bot (of which I no longer think exists, I will update with more info if I find it) which did much the same thing. Mechapod is by far the least professional thing that I have created, but it allows me a lot of freedom to make jokes and figure out creative ways in which to make those jokes.
+            </p>
+          </div>
         </div>
       </div>
-
+      <div class='w3-container w3-margin'>
+        <h1 class='w3-xlarge'>ryan@ryan-desktop:~$ ryan -contact </h1>
+        <p class='w3-large w3-margin-left'>
+          The best way by far to contact me is over email at rbuxton1@gmail.com. I am always glad to hear out and idea or concept, but no promises on whether or not I will be able to do something or be willing. When in doubt, shoot me an email!
+        </p>
+      </div>
 
       <!-- Footer -->
       <footer class="w3-container w3-theme-dark w3-padding-16">
